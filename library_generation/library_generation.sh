8ff2767b73f98c0ab394f2c72c0a75eff7a7b894
Pablo Garrido <pablogs9@gmail.com>
2021-11-19 10:57:24 +0100

    Add TF msgs

--- library_generation/library_generation.sh
+++ library_generation/library_generation.sh
@@ -28,6 +28,13 @@ echo $(which arm-none-eabi-gcc)
 echo Version: $(arm-none-eabi-gcc -dumpversion)
 echo "-------------"
 
+######## Add extra packages  ########
+pushd extra_packages
+    git clone -b ros2 https://github.com/ros2/geometry2
+    cp -R geometry2/tf2_msgs tf2_msgs
+    rm -rf geometry2
+popd
+
 ######## Build  ########
 
 if [ ! -f "$BASE_PATH/libmicroros/libmicroros.a" ]; then
