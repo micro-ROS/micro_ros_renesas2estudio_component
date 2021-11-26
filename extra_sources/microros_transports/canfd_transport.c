1167b224b0ef025a3861eaaf9f77b2d5ffaafeda
acuadros95 <acuadros1995@gmail.com>
2021-11-26 13:17:24 +0100

    Update can transport

--- extra_sources/microros_transports/canfd_transport.c
+++ extra_sources/microros_transports/canfd_transport.c
@@ -13,14 +13,18 @@
 
 #ifdef RMW_UXRCE_TRANSPORT_CUSTOM
 
+// Set device can identifier
+const uint32_t CAN_ID = 0x00001500;
+
+#define CAN_MAILBOX_NUMBER_0  0U
+#define CANFD_MTU 64
+
+uint8_t len_to_dlc(size_t len);
+
 // --- micro-ROS Transports ---
 bool g_write_complete = false;
-bool g_read_complete = false;
+bool g_error = false;
 
-// TODO: handle user CAN_ID
-const uint32_t CAN_ID = 0x1500;
-
-// TODO: Acceptance filter array parameters 
 const canfd_afl_entry_t p_canfd0_afl[CANFD_CFG_AFL_CH0_RULE_NUM] =
 {
  { // Accept all messages with Extended ID 0x1000-0x1FFF
@@ -35,9 +39,9 @@ const canfd_afl_entry_t p_canfd0_afl[CANFD_CFG_AFL_CH0_RULE_NUM] =
    .mask =
    {
     // These values mask which ID/mode bits to compare when filtering messages.
-    .mask_id         = 0x0000FF00,
-    .mask_frame_type = 0,
-    .mask_id_mode    = 1
+    .mask_id         = 0x1FFFFFFFU,
+    .mask_frame_type = CAN_FRAME_TYPE_DATA,
+    .mask_id_mode    = CAN_ID_MODE_EXTENDED
    },
 
    .destination =
@@ -55,20 +59,42 @@ const canfd_afl_entry_t p_canfd0_afl[CANFD_CFG_AFL_CH0_RULE_NUM] =
  },
 };
 
+uint8_t len_to_dlc(size_t len)
+{
+    uint8_t result = 0;
+
+    if (len<= 8)
+    {
+        result = (uint8_t) len;
+    }
+    else if (len<= 24)
+    {
+        len -= 8U;
+        result =  (uint8_t) (8U + (len / 4U));
+
+        if(len % 4U){
+            result++;
+        }
+    }
+    else if (len<= 64)
+    {
+        result = (uint8_t) (0xDU + ((len / 16U) - 2U));
+
+        if(len % 16U){
+            result++;
+        }
+    }
+
+    return result;
+}
+
 void canfd0_callback(can_callback_args_t *p_args)
 {
     switch (p_args->event)
     {
         case CAN_EVENT_TX_COMPLETE:
-        // case CAN_EVENT_TX_FIFO_EMPTY:
         {
             g_write_complete = true;
-            break;
-        }
-        // TODO: Check this comment
-        case CAN_EVENT_RX_COMPLETE: // Currently driver don't support this. This is unreachable code for now.
-        {
-            g_read_complete = true;
             break;
         }
         case CAN_EVENT_ERR_WARNING:             //error warning event
@@ -81,16 +107,25 @@ void canfd0_callback(can_callback_args_t *p_args)
         case CAN_EVENT_TX_ABORTED:              // Transmit abort event.
         case CAN_EVENT_ERR_GLOBAL:              // Global error has occurred.
         {
-            volatile canfd_error_t error = p_args->error;
+            // volatile canfd_error_t error = p_args->error;
+            g_error = true;
             break;
         }
+
+        case CAN_EVENT_RX_COMPLETE: // Currently driver don't support this. This is unreachable code for now.
+        case CAN_EVENT_TX_FIFO_EMPTY:
+        default:
+            break;
     }
 }
 
 bool renesas_e2_transport_open(struct uxrCustomTransport * transport){
     (void) transport;
 
-    // Use mailbox or FIFO?
+    if (UXR_CONFIG_CUSTOM_TRANSPORT_MTU != CANFD_MTU) {
+        return false;
+    }
+
     fsp_err_t err = R_CANFD_Open(&g_canfd0_ctrl, &g_canfd0_cfg);
     return err == FSP_SUCCESS;
 }
@@ -99,7 +134,6 @@ bool renesas_e2_transport_close(struct uxrCustomTransport * transport){
     (void) transport;
 
     fsp_err_t err = R_CANFD_Close(&g_canfd0_ctrl);
-
     return err == FSP_SUCCESS;
 }
 
@@ -111,21 +145,25 @@ size_t renesas_e2_transport_write(struct uxrCustomTransport* transport, const ui
     g_canfd_tx_frame.id = CAN_ID;
     g_canfd_tx_frame.id_mode = CAN_ID_MODE_EXTENDED,
     g_canfd_tx_frame.type = CAN_FRAME_TYPE_DATA;
-    g_canfd_tx_frame.data_length_code = (uint8_t) len;
+    g_canfd_tx_frame.data_length_code = len_to_dlc(len);
     g_canfd_tx_frame.options = CANFD_FRAME_OPTION_FD;
 
     g_write_complete = false;
-    memcpy(&g_canfd_tx_frame.data[0], buf, len);
-    fsp_err_t err =  R_CANFD_Write(&g_canfd0_ctrl, 0, &g_canfd_tx_frame);
+    g_error = false;
 
-    if (err != FSP_SUCCESS)
-    {
+    memcpy(&g_canfd_tx_frame.data[0], buf, len);
+    fsp_err_t err =  R_CANFD_Write(&g_canfd0_ctrl, CAN_MAILBOX_NUMBER_0, &g_canfd_tx_frame);
+
+    if (err != FSP_SUCCESS) {
         return 0;
     }
 
-    while(!g_write_complete)
-    {
+    while(!g_write_complete) {
         R_BSP_SoftwareDelay(10, BSP_DELAY_UNITS_MICROSECONDS);
+
+        if (g_error) {
+            return 0;
+        }
     }
 
     return len;
@@ -141,8 +179,9 @@ size_t renesas_e2_transport_read(struct uxrCustomTransport* transport, uint8_t* 
 
     int64_t start = uxr_millis();
     size_t wrote = 0;
+    g_error = false;
 
-    while ((uxr_millis() -  start) < timeout)
+    while ((uxr_millis() -  start) < timeout && !g_error)
     {
         err = R_CANFD_InfoGet(&g_canfd0_ctrl, &can_rx_info);
 
@@ -154,8 +193,8 @@ size_t renesas_e2_transport_read(struct uxrCustomTransport* transport, uint8_t* 
 
         if(can_rx_info.rx_mb_status)
         {
-            /* Read the input frame received */
-            err = R_CANFD_Read(&g_canfd0_ctrl, 0, &g_canfd_rx_frame);
+            // Read the input frame received
+            err = R_CANFD_Read(&g_canfd0_ctrl, CANFD_RX_MB_0, &g_canfd_rx_frame);
 
             if (FSP_SUCCESS != err)
             {
@@ -163,8 +202,11 @@ size_t renesas_e2_transport_read(struct uxrCustomTransport* transport, uint8_t* 
                 return 0;
             }
 
-            // TODO: check len
-            (void) len;
+            if (g_canfd_rx_frame.data_length_code > len) {
+                // This should never execute
+                return 0;
+            }
+
             memcpy(buf, &g_canfd_rx_frame.data[0], g_canfd_rx_frame.data_length_code);
             wrote = g_canfd_rx_frame.data_length_code;
             break;
