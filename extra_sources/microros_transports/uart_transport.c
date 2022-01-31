8d7a972983a99f0f58b59219b0624d9115867407
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2022-01-31 12:34:12 +0100

    Add serial support for RA6T2 board (#31)
    * Update UART transport
    
    * Update README with RA6T2
    
    * Update boards table
    
    * Update transports with TCP
    
    * Update transports with RA6T2
    
    * Remove ':'
    
    * Comment CANFD for new board

--- extra_sources/microros_transports/uart_transport.c
+++ extra_sources/microros_transports/uart_transport.c
@@ -28,8 +28,12 @@ void user_uart_callback (uart_callback_args_t * p_args)
             break;
 
         case UART_EVENT_RX_COMPLETE:
-        	 R_SCI_UART_Read(&g_uart0_ctrl, &it_buffer[0], UART_IT_BUFFER_SIZE);
-        	 break;
+        #if defined (BOARD_RA6T2_MCK)
+            R_SCI_B_UART_Read(&g_uart0_ctrl, &it_buffer[0], UART_IT_BUFFER_SIZE);
+        #else
+            R_SCI_UART_Read(&g_uart0_ctrl, &it_buffer[0], UART_IT_BUFFER_SIZE);
+        #endif
+            break;
 
         default:
             break;
@@ -38,16 +42,28 @@ void user_uart_callback (uart_callback_args_t * p_args)
 
 bool renesas_e2_transport_open(struct uxrCustomTransport * transport){
     (void) transport;
+    fsp_err_t err;
 
-    fsp_err_t err = R_SCI_UART_Open(&g_uart0_ctrl, &g_uart0_cfg);
+#if defined (BOARD_RA6T2_MCK)
+    R_SCI_B_UART_Open(&g_uart0_ctrl, &g_uart0_cfg);
+    err = R_SCI_B_UART_Read(&g_uart0_ctrl, &it_buffer[0], UART_IT_BUFFER_SIZE);
+#else
+    R_SCI_UART_Open(&g_uart0_ctrl, &g_uart0_cfg);
     err = R_SCI_UART_Read(&g_uart0_ctrl, &it_buffer[0], UART_IT_BUFFER_SIZE);
+#endif
+
     return err == FSP_SUCCESS;
 }
 
 bool renesas_e2_transport_close(struct uxrCustomTransport * transport){
     (void) transport;
+    fsp_err_t err;
 
-    fsp_err_t err = R_SCI_UART_Close(&g_uart0_ctrl);
+#if defined (BOARD_RA6T2_MCK)
+    err = R_SCI_B_UART_Close(&g_uart0_ctrl);
+#else
+    err = R_SCI_UART_Close(&g_uart0_ctrl);
+#endif
 
     return err == FSP_SUCCESS;
 }
@@ -57,7 +73,13 @@ size_t renesas_e2_transport_write(struct uxrCustomTransport* transport, const ui
     (void) error;
 
     g_write_complete = false;
-    fsp_err_t err = R_SCI_UART_Write(&g_uart0_ctrl, buf, len);
+    fsp_err_t err;
+
+#if defined (BOARD_RA6T2_MCK)
+    err = R_SCI_B_UART_Write(&g_uart0_ctrl, buf, len);
+#else
+    err = R_SCI_UART_Write(&g_uart0_ctrl, buf, len);
+#endif
 
     if (err != FSP_SUCCESS)
     {
