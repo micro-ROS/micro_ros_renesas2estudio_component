63baee89c07cc8096ce1c96f6aa87f0d882880bf
Pablo Garrido <pablogs9@gmail.com>
2022-03-30 10:36:02 +0200

    Fix Rolling includes
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- library_generation/library_generation.sh
+++ library_generation/library_generation.sh
@@ -47,9 +47,13 @@ else
 fi
 
 ######## Fix include paths  ########
-INCLUDE_ROS2_PACKAGES=( rmw rcl rcl_action rcl_lifecycle rcl_logging_interface rosidl_runtime_c rosidl_typesupport_interface)
-for var in "${INCLUDE_ROS2_PACKAGES[@]}"; do
-    mv $BASE_PATH/libmicroros/include/${var}/${var}/* $BASE_PATH/libmicroros/include/${var}
+pushd $BASE_PATH/libmicroros/micro_ros_src > /dev/null
+    INCLUDE_ROS2_PACKAGES=$(colcon list | awk '{print $1}' | awk -v d=" " '{s=(NR==1?s:s d)$0}END{print s}')
+popd > /dev/null
+
+apt -y install rsync
+for var in ${INCLUDE_ROS2_PACKAGES}; do
+    rsync -r $BASE_PATH/libmicroros/include/${var}/${var}/* $BASE_PATH/libmicroros/include/${var}
     rm -rf $BASE_PATH/libmicroros/include/${var}/${var}
 done
 
