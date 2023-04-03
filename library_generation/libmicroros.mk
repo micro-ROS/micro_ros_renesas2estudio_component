1b343df229204b1a3bff400d55b06234972646bd
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2023-04-03 12:25:05 +0200

    Update rolling sources (#74)
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

--- library_generation/libmicroros.mk
+++ library_generation/libmicroros.mk
@@ -32,12 +32,12 @@ $(INSTALL_DIR)/toolchain.cmake: $(COMPONENT_DIR)/toolchain.cmake.in
 $(INSTALL_DIR)/micro_ros_dev/install:
 	rm -rf $(INSTALL_DIR)/micro_ros_dev; \
 	mkdir $(INSTALL_DIR)/micro_ros_dev; cd $(INSTALL_DIR)/micro_ros_dev; \
-	git clone -b master https://github.com/ament/ament_cmake src/ament_cmake; \
-	git clone -b master https://github.com/ament/ament_lint src/ament_lint; \
-	git clone -b master https://github.com/ament/ament_package src/ament_package; \
-	git clone -b ros2 https://github.com/ament/googletest src/googletest; \
-	git clone -b master https://github.com/ros2/ament_cmake_ros src/ament_cmake_ros; \
-	git clone -b master https://github.com/ament/ament_index src/ament_index; \
+	git clone -b rolling https://github.com/ament/ament_cmake src/ament_cmake; \
+	git clone -b rolling https://github.com/ament/ament_lint src/ament_lint; \
+	git clone -b rolling https://github.com/ament/ament_package src/ament_package; \
+	git clone -b rolling https://github.com/ament/googletest src/googletest; \
+	git clone -b rolling https://github.com/ros2/ament_cmake_ros src/ament_cmake_ros; \
+	git clone -b rolling https://github.com/ament/ament_index src/ament_index; \
 	colcon build --cmake-args -DBUILD_TESTING=OFF;
 
 $(INSTALL_DIR)/micro_ros_src/src:
@@ -52,15 +52,15 @@ $(INSTALL_DIR)/micro_ros_src/src:
 	git clone -b main https://github.com/micro-ROS/rmw-microxrcedds src/rmw-microxrcedds; \
 	git clone -b master https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
 	git clone -b main https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
-	git clone -b master https://github.com/ros2/rosidl src/rosidl; \
-	git clone -b master https://github.com/ros2/rmw src/rmw; \
-	git clone -b master https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
-	git clone -b master https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \
-	git clone -b master https://github.com/ros2/unique_identifier_msgs src/unique_identifier_msgs; \
-	git clone -b master https://github.com/ros2/common_interfaces src/common_interfaces; \
-	git clone -b master https://github.com/ros2/test_interface_files src/test_interface_files; \
-	git clone -b master https://github.com/ros2/rmw_implementation src/rmw_implementation; \
-	git clone -b master https://github.com/ros2/rcl_logging src/rcl_logging; \
+	git clone -b rolling https://github.com/ros2/rosidl src/rosidl; \
+	git clone -b rolling https://github.com/ros2/rmw src/rmw; \
+	git clone -b rolling https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
+	git clone -b rolling https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \
+	git clone -b rolling https://github.com/ros2/unique_identifier_msgs src/unique_identifier_msgs; \
+	git clone -b rolling https://github.com/ros2/common_interfaces src/common_interfaces; \
+	git clone -b rolling https://github.com/ros2/test_interface_files src/test_interface_files; \
+	git clone -b rolling https://github.com/ros2/rmw_implementation src/rmw_implementation; \
+	git clone -b rolling https://github.com/ros2/rcl_logging src/rcl_logging; \
 	git clone -b master https://gitlab.com/micro-ROS/ros_tracing/ros2_tracing src/ros2_tracing; \
 	git clone -b main https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
 	git clone -b rolling https://github.com/ros2/rosidl_core src/rosidl_core; \
