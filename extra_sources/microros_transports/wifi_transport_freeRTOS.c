5940879e0d4bb6fbcdf53fdee34c3df5cbdd43cd
acuadros95 <acuadros1995@gmail.com>
2023-06-28 13:08:34 +0200

    Update WiFi transport
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

--- extra_sources/microros_transports/wifi_transport_freeRTOS.c
+++ extra_sources/microros_transports/wifi_transport_freeRTOS.c
@@ -13,9 +13,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-#include "aws_secure_sockets_config.h"
-#include "iot_secure_sockets.h"
-
+#include "sockets_wrapper.h"
 
 typedef enum {
   STATE_WAIT_FOR_SIZE = 0,
@@ -44,7 +42,6 @@ Socket_t xSocket;
 bool renesas_e2_transport_open(struct uxrCustomTransport * transport) {
     custom_transport_args * args = (custom_transport_args*) transport->args;
     WIFINetworkParams_t * network_conf = (WIFINetworkParams_t *) args->network_conf;
-    SocketsSockaddr_t * socket_addr = (SocketsSockaddr_t *) args->socket_addr;
 
     // Set network struct length values. TODO: could strlen be problematic?
     network_conf->xPassword.xWPA.ucLength = (uint8_t) strlen((char *) network_conf->xPassword.xWPA.cPassphrase);
@@ -67,47 +64,26 @@ bool renesas_e2_transport_open(struct uxrCustomTransport * transport) {
         return false;
     }
 
-    // Initialize the Socket Interface
-    BaseType_t sock_err;
-    if(!net_init)
-    {
-        sock_err = SOCKETS_Init();
-
-        if (pdPASS != sock_err)
-        {
-            // TODO: use assert(pdPASS == sock_err); ?
-            return false;
-        }
-
-        net_init = true;
-    }
-
-    // Create socket
-    xSocket = SOCKETS_Socket(SOCKETS_AF_INET, SOCKETS_SOCK_STREAM, SOCKETS_IPPROTO_TCP);
-
-    if(SOCKETS_INVALID_SOCKET == xSocket)
-    {
-        return false;
-    }
+    net_init = true;
 
     // Connect to agent TCP server
-    sock_err = SOCKETS_Connect(xSocket, socket_addr, sizeof(SocketsSockaddr_t));
+    BaseType_t sock_err = Sockets_Connect(&xSocket,
+                           args->agent_ip,
+                           args->agent_port,
+                           100,
+                           WRITE_TIMEOUT);
 
     if (SOCKETS_ERROR_NONE != sock_err)
     {
         return false;
     }
 
-    // Set write timeout
-    TickType_t timeout_ticks = pdMS_TO_TICKS(WRITE_TIMEOUT);
-    SOCKETS_SetSockOpt(xSocket, 0, SOCKETS_SO_SNDTIMEO, &timeout_ticks, 0);
-
     return true;
 }
 
 bool renesas_e2_transport_close(struct uxrCustomTransport * transport) {
     (void) transport;
-    (void) SOCKETS_Close(xSocket);
+    Sockets_Disconnect(xSocket);
 
     if (wifi_init && eWiFiSuccess == WIFI_Off())
     {
@@ -127,12 +103,12 @@ size_t renesas_e2_transport_write(struct uxrCustomTransport* transport, const ui
     static uint8_t buffer_size[2];
     buffer_size[0] = (uint8_t)(0x00FF & len);
     buffer_size[1] = (uint8_t)((0xFF00 & len) >> 8);
-    bytes_sent = SOCKETS_Send(xSocket, (const void *) &buffer_size[0], 2, 0);
+    bytes_sent = Sockets_Send(xSocket, (const void *) &buffer_size[0], 2);
 
     // Then we send the payload
     if (bytes_sent == 2)
     {
-        bytes_sent = SOCKETS_Send(xSocket, (const void *) &buf[0], len, 0);
+        bytes_sent = Sockets_Send(xSocket, (const void *) &buf[0], len);
 
         if (0 <= bytes_sent)
         {
@@ -154,22 +130,14 @@ size_t renesas_e2_transport_write(struct uxrCustomTransport* transport, const ui
 
 size_t renesas_e2_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err) {
     (void) transport;
+    (void) timeout;
     (void) len;
 
     static tcp_receiver_t receiver = {};
     size_t rv = 0;
 
-    if (timeout == 0)
-    {
-    	  timeout = 1;
-    }
-
     do
     {
-        // Set read timeout
-        TickType_t timeout_ticks = pdMS_TO_TICKS(timeout);
-        SOCKETS_SetSockOpt(xSocket, 0, SOCKETS_SO_RCVTIMEO, &timeout_ticks, 0);
-
         int64_t time_init = uxr_millis();
         read_tcp_data(&receiver);
         timeout -= (int)(uxr_millis() - time_init);
@@ -200,7 +168,7 @@ void read_tcp_data(tcp_receiver_t * receiver) {
   switch(receiver->state)
   {
     case STATE_WAIT_FOR_SIZE:
-      bytes_received = SOCKETS_Recv(xSocket, (void*) &receiver->length_buffer[0], 2, 0);
+      bytes_received = Sockets_Recv(xSocket, (void*) &receiver->length_buffer[0], 2);
       if (bytes_received >= 2)
       {
         receiver->message_size = (uint16_t)(receiver->length_buffer[0] | (receiver->length_buffer[1] << 8));
@@ -211,7 +179,7 @@ void read_tcp_data(tcp_receiver_t * receiver) {
 
     case STATE_WAIT_FOR_DATA:
       to_read = receiver->message_size - receiver->message_size_received;
-      bytes_received = SOCKETS_Recv(xSocket, (void*) &receiver->buffer[receiver->message_size_received], to_read, 0);
+      bytes_received = Sockets_Recv(xSocket, (void*) &receiver->buffer[receiver->message_size_received], to_read);
       receiver->message_size_received += (uint16_t) bytes_received;
 
       if(receiver->message_size_received == receiver->message_size){
